1015d3441656d53c2693846a4058f1929e7604a6
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-04 13:13:16 +0000

    done

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -13,15 +13,28 @@ const TextToSpeechPage = () => {
     }
   };
 
-  return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-        <div className='w-1/2 h-96 flex justify-center items-center shadow-xl shadow-zinc-700 rounded-xl'>
-      <h1>Text-to-Speech in Next.js</h1>
-      <button onClick={() => speakPredefinedText('Hello, this is a predefined message.')}>
-        Speak Predefined Text
+  return (<>
+    <div className='w-100vw h-32 flex justify-center items-center font-extrabold text-3xl'>Text to Speech Converter</div>
+    <div className='flex w-screen h-3/4  justify-center items-center'>
+        
+        <div className='w-1/2  h-110  shadow-xl shadow-blue-500 rounded-xl '>
+        <div className='w-full h-8 bg-sky-500'></div>
+          <div className='w-full h-16 flex justify-center items-center'>
+        <img src='../speak.svg'  className='w-16 h-16'/ >
+        </div>
+        <div>
+      <h1 className='text-xl px-16'>Web development, an ever-evolving field, plays a central role in the digital transformation of our world. It encompasses two core aspects—front-end development and back-end development—each contributing to the dynamic and interconnected online environment we engage with every day. Developers harness a range of technologies and tools to create websites and applications that are not only visually appealing but also highly functional. This essay delves into the essence of web development and its significance in our interconnected age.</h1>
+      </div>
+      
+      </div>
+     
+    </div>
+    <div className='w-100vw h-16 flex justify-center items-center'>
+    <button className='w-48 h-12 bg-sky-500 hover:bg-sky-700 rounded-md text-white' onClick={() => speakPredefinedText('Web development, an ever-evolving field, plays a central role in the digital transformation of our world. It encompasses two core aspects—front-end development and back-end development—each contributing to the dynamic and interconnected online environment we engage with every day. Developers harness a range of technologies and tools to create websites and applications that are not only visually appealing but also highly functional. This essay delves into the essence of web development and its significance in our interconnected age.')}>
+        Speak
       </button>
       </div>
-    </div>
+    </>
   );
 };
 
